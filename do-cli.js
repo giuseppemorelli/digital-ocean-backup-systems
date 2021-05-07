20a1bcb4ffbd31391787e113a6262be5cfbaf682
Giuseppe Morelli <hello@giuseppemorelli.net>
2021-05-07 20:11:15 +0200

    improvement code of copy snapshot (#3)

--- do-cli.js
+++ do-cli.js
@@ -1,5 +1,11 @@
 import { createSnapshot } from "./src/create-snapshot.js";
 import { copySnapshot } from "./src/copy-snapshot.js";
 
+console.log("*********************************");
+console.log("**      CREATE SNAPSHOT        **");
+console.log("*********************************");
 createSnapshot();
+console.log("*********************************");
+console.log("**       COPY SNAPSHOT         **");
+console.log("*********************************");
 copySnapshot();
